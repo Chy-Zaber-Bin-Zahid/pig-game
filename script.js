2a1eb33689d12629a8b84103993c195b4e1c6cd0
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2022-10-13 18:32:38 +0600

    added dice and some functionality to add score except one

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0EL.textContent = currentScore;
+  } else {
+  }
+});
